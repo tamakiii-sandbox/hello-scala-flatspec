9171b7071db77af8525078b406dc321ef067a752
tamakiii <tamakiii@users.noreply.github.com>
2024-07-23 01:52:11 +0900

    HttpServletRequestParser.parse

--- src/main/scala/servlet/Index.scala
+++ src/main/scala/servlet/Index.scala
@@ -3,12 +3,14 @@ package com.example.main.servlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import com.example.main.HttpServletRequestParser
 
 class Index extends HttpServlet {
   override def doGet(
       req: HttpServletRequest,
       resp: HttpServletResponse
   ): Unit = {
+    val request = HttpServletRequestParser.parse(req)
     resp.setContentType("text/plain")
     resp.getWriter.println("Hello, World!")
   }
